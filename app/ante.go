sideprotocol/side

add fee sponsorship for BTCT transfer

app/ante.go
@@ -1,0 +1,66 @@
+package app
+
+import (
+	errorsmod "cosmossdk.io/errors"
+	storetypes "cosmossdk.io/store/types"
+	feegrantkeeper "cosmossdk.io/x/feegrant/keeper"
+	txsigning "cosmossdk.io/x/tx/signing"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/cosmos/cosmos-sdk/types/tx/signing"
+	"github.com/cosmos/cosmos-sdk/x/auth/ante"
+	authkeeper "github.com/cosmos/cosmos-sdk/x/auth/keeper"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	bankkeeper "github.com/cosmos/cosmos-sdk/x/bank/keeper"
+
+	btcbridgeante "github.com/sideprotocol/side/x/btcbridge/ante"
+	btcbridgekeeper "github.com/sideprotocol/side/x/btcbridge/keeper"
+)
+
+// HandlerOptions defines the options required for constructing an AnteHandler.
+type HandlerOptions struct {
+	AccountKeeper          *authkeeper.AccountKeeper
+	BankKeeper             bankkeeper.Keeper
+	FeegrantKeeper         *feegrantkeeper.Keeper
+	BtcBridgeKeeper        *btcbridgekeeper.Keeper
+	ExtensionOptionChecker ante.ExtensionOptionChecker
+	SignModeHandler        *txsigning.HandlerMap
+	SigGasConsumer         func(meter storetypes.GasMeter, sig signing.SignatureV2, params authtypes.Params) error
+	TxFeeChecker           ante.TxFeeChecker
+}
+
+// NewAnteHandler returns an AnteHandler similar to the default SDK AnteHandler except that fee sponsorship is supported
+func NewAnteHandler(options HandlerOptions) (sdk.AnteHandler, error) {
+	if options.AccountKeeper == nil {
+		return nil, errorsmod.Wrap(sdkerrors.ErrLogic, "account keeper is required for ante builder")
+	}
+
+	if options.BankKeeper == nil {
+		return nil, errorsmod.Wrap(sdkerrors.ErrLogic, "bank keeper is required for ante builder")
+	}
+
+	if options.BtcBridgeKeeper == nil {
+		return nil, errorsmod.Wrap(sdkerrors.ErrLogic, "btcbridge keeper is required for ante builder")
+	}
+
+	if options.SignModeHandler == nil {
+		return nil, errorsmod.Wrap(sdkerrors.ErrLogic, "sign mode handler is required for ante builder")
+	}
+
+	anteDecorators := []sdk.AnteDecorator{
+		ante.NewSetUpContextDecorator(), // outermost AnteDecorator. SetUpContext must be called first
+		ante.NewExtensionOptionsDecorator(options.ExtensionOptionChecker),
+		ante.NewValidateBasicDecorator(),
+		ante.NewTxTimeoutHeightDecorator(),
+		ante.NewValidateMemoDecorator(options.AccountKeeper),
+		ante.NewConsumeGasForTxSizeDecorator(options.AccountKeeper),
+		btcbridgeante.NewFeeSponsorshipDecorator(options.AccountKeeper, options.BankKeeper, options.FeegrantKeeper, options.BtcBridgeKeeper, options.TxFeeChecker),
+		ante.NewSetPubKeyDecorator(options.AccountKeeper), // SetPubKeyDecorator must be called before all signature verification decorators
+		ante.NewValidateSigCountDecorator(options.AccountKeeper),
+		ante.NewSigGasConsumeDecorator(options.AccountKeeper, options.SigGasConsumer),
+		ante.NewSigVerificationDecorator(options.AccountKeeper, options.SignModeHandler),
+		ante.NewIncrementSequenceDecorator(options.AccountKeeper),
+	}
+
+	return sdk.ChainAnteDecorators(anteDecorators...), nil
+}

x/btcbridge/ante/fee_sponsorship.go
@@ -1,0 +1,164 @@
+package ante
+
+import (
+	errorsmod "cosmossdk.io/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	authante "github.com/cosmos/cosmos-sdk/x/auth/ante"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+
+	"github.com/sideprotocol/side/x/btcbridge/types"
+)
+
+// FeeSponsorshipDecorator implements fee sponsorship
+// The gas fee is sponsored if the tx satisfies the sponsorship requirement
+// Fallback to the default DeductFeeDecorator otherwise
+type FeeSponsorshipDecorator struct {
+	accountKeeper   types.AccountKeeper
+	bankKeeper      types.BankKeeper
+	feegrantKeeper  FeeGrantKeeper
+	btcbridgeKeeper BtcBridgeKeeper
+
+	txFeeChecker authante.TxFeeChecker
+
+	defaultFeeDecorator authante.DeductFeeDecorator
+}
+
+// NewFeeSponsorshipDecorator creates a new decorator for fee sponsorship
+func NewFeeSponsorshipDecorator(authKeeper types.AccountKeeper, bankKeeper types.BankKeeper, feegrantKeeper FeeGrantKeeper, btcbridgeKeeper BtcBridgeKeeper, txFeeChecker authante.TxFeeChecker) FeeSponsorshipDecorator {
+	if txFeeChecker == nil {
+		txFeeChecker = checkTxFeeWithValidatorMinGasPrices
+	}
+
+	return FeeSponsorshipDecorator{
+		accountKeeper:       authKeeper,
+		bankKeeper:          bankKeeper,
+		feegrantKeeper:      feegrantKeeper,
+		btcbridgeKeeper:     btcbridgeKeeper,
+		txFeeChecker:        txFeeChecker,
+		defaultFeeDecorator: authante.NewDeductFeeDecorator(authKeeper, bankKeeper, feegrantKeeper, txFeeChecker),
+	}
+}
+
+// AnteHandle implements sdk.AnteDecorator
+func (fsd FeeSponsorshipDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (sdk.Context, error) {
+	feeTx, ok := tx.(sdk.FeeTx)
+	if !ok {
+		return ctx, errorsmod.Wrap(sdkerrors.ErrTxDecode, "Tx must be a FeeTx")
+	}
+
+	if !simulate && ctx.BlockHeight() > 0 && feeTx.GetGas() == 0 {
+		return ctx, errorsmod.Wrap(sdkerrors.ErrInvalidGasLimit, "must provide positive gas")
+	}
+
+	var (
+		priority int64
+		err      error
+	)
+
+	fee := feeTx.GetFee()
+	if !simulate {
+		fee, priority, err = fsd.txFeeChecker(ctx, tx)
+		if err != nil {
+			return ctx, err
+		}
+	}
+
+	if !fsd.checkSponsorship(ctx, tx, fee) {
+		// fallback to default fee decorator
+		return fsd.defaultFeeDecorator.AnteHandle(ctx, tx, simulate, next)
+	}
+
+	// sponsor fee
+	if err := fsd.sponsorFee(ctx, fee); err != nil {
+		return ctx, err
+	}
+
+	newCtx := ctx.WithPriority(priority)
+
+	return next(newCtx, tx, simulate)
+}
+
+// sponsorFee performs the fee sponsorship for the given tx
+func (fsd FeeSponsorshipDecorator) sponsorFee(ctx sdk.Context, fee sdk.Coins) error {
+	sponsorAddress := fsd.getFeeSponsorAddress()
+
+	sponsorAccount := fsd.accountKeeper.GetAccount(ctx, sponsorAddress)
+	if sponsorAccount == nil {
+		return sdkerrors.ErrUnknownAddress.Wrapf("fee sponsor address: %s does not exist", sponsorAddress)
+	}
+
+	// deduct fees from the sponsor account
+	if !fee.IsZero() {
+		err := authante.DeductFees(fsd.bankKeeper, ctx, sponsorAccount, fee)
+		if err != nil {
+			return err
+		}
+	}
+
+	events := sdk.Events{
+		sdk.NewEvent(
+			sdk.EventTypeTx,
+			sdk.NewAttribute(sdk.AttributeKeyFee, fee.String()),
+			sdk.NewAttribute(sdk.AttributeKeyFeePayer, sponsorAddress.String()),
+		),
+	}
+	ctx.EventManager().EmitEvents(events)
+
+	return nil
+}
+
+// checkSponsorship returns true if the given tx should be sponsored, false otherwise
+// NOTE: The following requirements must be satisfied for fee sponsorship
+// 1. Fee sponsorship is enabled
+// 2. The tx is a BTCT transfer tx
+// 3. The gas fee does not exceed the maximum sponsorship fee
+// 4. The fee sponsor account has sufficient balances
+func (fsd FeeSponsorshipDecorator) checkSponsorship(ctx sdk.Context, tx sdk.Tx, fee sdk.Coins) bool {
+	if !fsd.btcbridgeKeeper.FeeSponsorshipEnabled(ctx) {
+		return false
+	}
+
+	if !fsd.isSponsorableTx(ctx, tx) {
+		return false
+	}
+
+	if !fee.IsAllLTE(fsd.btcbridgeKeeper.MaxSponsorFee(ctx)) {
+		return false
+	}
+
+	if !fsd.bankKeeper.SpendableCoins(ctx, fsd.getFeeSponsorAddress()).IsAllGTE(fee) {
+		return false
+	}
+
+	return true
+}
+
+// isSponsorableTx returns true if the given tx is sponsorable, false otherwise
+func (fsd FeeSponsorshipDecorator) isSponsorableTx(ctx sdk.Context, tx sdk.Tx) bool {
+	for _, m := range tx.GetMsgs() {
+		switch msg := m.(type) {
+		case *banktypes.MsgSend:
+			denoms := msg.Amount.Denoms()
+			if len(denoms) != 1 || denoms[0] != fsd.btcbridgeKeeper.BtcDenom(ctx) {
+				return false
+			}
+
+		case *banktypes.MsgMultiSend:
+			denoms := msg.Inputs[0].Coins.Denoms()
+			if len(denoms) != 1 || denoms[0] != fsd.btcbridgeKeeper.BtcDenom(ctx) {
+				return false
+			}
+
+		default:
+			return false
+		}
+	}
+
+	return true
+}
+
+// getFeeSponsorAddress gets the fee sponsor address
+func (fsd FeeSponsorshipDecorator) getFeeSponsorAddress() sdk.AccAddress {
+	return fsd.accountKeeper.GetModuleAddress(types.FeeSponsorName)
+}

bitwaylabs/bitway

app/ante.go
@@ -1,0 +1,66 @@
+package app
+
+import (
+	errorsmod "cosmossdk.io/errors"
+	storetypes "cosmossdk.io/store/types"
+	feegrantkeeper "cosmossdk.io/x/feegrant/keeper"
+	txsigning "cosmossdk.io/x/tx/signing"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/cosmos/cosmos-sdk/types/tx/signing"
+	"github.com/cosmos/cosmos-sdk/x/auth/ante"
+	authkeeper "github.com/cosmos/cosmos-sdk/x/auth/keeper"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	bankkeeper "github.com/cosmos/cosmos-sdk/x/bank/keeper"
+
+	btcbridgeante "github.com/bitwaylabs/bitway/x/btcbridge/ante"
+	btcbridgekeeper "github.com/bitwaylabs/bitway/x/btcbridge/keeper"
+)
+
+// HandlerOptions defines the options required for constructing an AnteHandler.
+type HandlerOptions struct {
+	AccountKeeper          *authkeeper.AccountKeeper
+	BankKeeper             bankkeeper.Keeper
+	FeegrantKeeper         *feegrantkeeper.Keeper
+	BtcBridgeKeeper        *btcbridgekeeper.Keeper
+	ExtensionOptionChecker ante.ExtensionOptionChecker
+	SignModeHandler        *txsigning.HandlerMap
+	SigGasConsumer         func(meter storetypes.GasMeter, sig signing.SignatureV2, params authtypes.Params) error
+	TxFeeChecker           ante.TxFeeChecker
+}
+
+// NewAnteHandler returns an AnteHandler similar to the default SDK AnteHandler except that fee sponsorship is supported
+func NewAnteHandler(options HandlerOptions) (sdk.AnteHandler, error) {
+	if options.AccountKeeper == nil {
+		return nil, errorsmod.Wrap(sdkerrors.ErrLogic, "account keeper is required for ante builder")
+	}
+
+	if options.BankKeeper == nil {
+		return nil, errorsmod.Wrap(sdkerrors.ErrLogic, "bank keeper is required for ante builder")
+	}
+
+	if options.BtcBridgeKeeper == nil {
+		return nil, errorsmod.Wrap(sdkerrors.ErrLogic, "btcbridge keeper is required for ante builder")
+	}
+
+	if options.SignModeHandler == nil {
+		return nil, errorsmod.Wrap(sdkerrors.ErrLogic, "sign mode handler is required for ante builder")
+	}
+
+	anteDecorators := []sdk.AnteDecorator{
+		ante.NewSetUpContextDecorator(), // outermost AnteDecorator. SetUpContext must be called first
+		ante.NewExtensionOptionsDecorator(options.ExtensionOptionChecker),
+		ante.NewValidateBasicDecorator(),
+		ante.NewTxTimeoutHeightDecorator(),
+		ante.NewValidateMemoDecorator(options.AccountKeeper),
+		ante.NewConsumeGasForTxSizeDecorator(options.AccountKeeper),
+		btcbridgeante.NewFeeSponsorshipDecorator(options.AccountKeeper, options.BankKeeper, options.FeegrantKeeper, options.BtcBridgeKeeper, options.TxFeeChecker),
+		ante.NewSetPubKeyDecorator(options.AccountKeeper), // SetPubKeyDecorator must be called before all signature verification decorators
+		ante.NewValidateSigCountDecorator(options.AccountKeeper),
+		ante.NewSigGasConsumeDecorator(options.AccountKeeper, options.SigGasConsumer),
+		ante.NewSigVerificationDecorator(options.AccountKeeper, options.SignModeHandler),
+		ante.NewIncrementSequenceDecorator(options.AccountKeeper),
+	}
+
+	return sdk.ChainAnteDecorators(anteDecorators...), nil
+}

x/btcbridge/ante/fee_sponsorship.go
@@ -1,0 +1,164 @@
+package ante
+
+import (
+	errorsmod "cosmossdk.io/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	authante "github.com/cosmos/cosmos-sdk/x/auth/ante"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+
+	"github.com/bitwaylabs/bitway/x/btcbridge/types"
+)
+
+// FeeSponsorshipDecorator implements fee sponsorship
+// The gas fee is sponsored if the tx satisfies the sponsorship requirement
+// Fallback to the default DeductFeeDecorator otherwise
+type FeeSponsorshipDecorator struct {
+	accountKeeper   types.AccountKeeper
+	bankKeeper      types.BankKeeper
+	feegrantKeeper  FeeGrantKeeper
+	btcbridgeKeeper BtcBridgeKeeper
+
+	txFeeChecker authante.TxFeeChecker
+
+	defaultFeeDecorator authante.DeductFeeDecorator
+}
+
+// NewFeeSponsorshipDecorator creates a new decorator for fee sponsorship
+func NewFeeSponsorshipDecorator(authKeeper types.AccountKeeper, bankKeeper types.BankKeeper, feegrantKeeper FeeGrantKeeper, btcbridgeKeeper BtcBridgeKeeper, txFeeChecker authante.TxFeeChecker) FeeSponsorshipDecorator {
+	if txFeeChecker == nil {
+		txFeeChecker = checkTxFeeWithValidatorMinGasPrices
+	}
+
+	return FeeSponsorshipDecorator{
+		accountKeeper:       authKeeper,
+		bankKeeper:          bankKeeper,
+		feegrantKeeper:      feegrantKeeper,
+		btcbridgeKeeper:     btcbridgeKeeper,
+		txFeeChecker:        txFeeChecker,
+		defaultFeeDecorator: authante.NewDeductFeeDecorator(authKeeper, bankKeeper, feegrantKeeper, txFeeChecker),
+	}
+}
+
+// AnteHandle implements sdk.AnteDecorator
+func (fsd FeeSponsorshipDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (sdk.Context, error) {
+	feeTx, ok := tx.(sdk.FeeTx)
+	if !ok {
+		return ctx, errorsmod.Wrap(sdkerrors.ErrTxDecode, "Tx must be a FeeTx")
+	}
+
+	if !simulate && ctx.BlockHeight() > 0 && feeTx.GetGas() == 0 {
+		return ctx, errorsmod.Wrap(sdkerrors.ErrInvalidGasLimit, "must provide positive gas")
+	}
+
+	var (
+		priority int64
+		err      error
+	)
+
+	fee := feeTx.GetFee()
+	if !simulate {
+		fee, priority, err = fsd.txFeeChecker(ctx, tx)
+		if err != nil {
+			return ctx, err
+		}
+	}
+
+	if !fsd.checkSponsorship(ctx, tx, fee) {
+		// fallback to default fee decorator
+		return fsd.defaultFeeDecorator.AnteHandle(ctx, tx, simulate, next)
+	}
+
+	// sponsor fee
+	if err := fsd.sponsorFee(ctx, fee); err != nil {
+		return ctx, err
+	}
+
+	newCtx := ctx.WithPriority(priority)
+
+	return next(newCtx, tx, simulate)
+}
+
+// sponsorFee performs the fee sponsorship for the given tx
+func (fsd FeeSponsorshipDecorator) sponsorFee(ctx sdk.Context, fee sdk.Coins) error {
+	sponsorAddress := fsd.getFeeSponsorAddress()
+
+	sponsorAccount := fsd.accountKeeper.GetAccount(ctx, sponsorAddress)
+	if sponsorAccount == nil {
+		return sdkerrors.ErrUnknownAddress.Wrapf("fee sponsor address: %s does not exist", sponsorAddress)
+	}
+
+	// deduct fees from the sponsor account
+	if !fee.IsZero() {
+		err := authante.DeductFees(fsd.bankKeeper, ctx, sponsorAccount, fee)
+		if err != nil {
+			return err
+		}
+	}
+
+	events := sdk.Events{
+		sdk.NewEvent(
+			sdk.EventTypeTx,
+			sdk.NewAttribute(sdk.AttributeKeyFee, fee.String()),
+			sdk.NewAttribute(sdk.AttributeKeyFeePayer, sponsorAddress.String()),
+		),
+	}
+	ctx.EventManager().EmitEvents(events)
+
+	return nil
+}
+
+// checkSponsorship returns true if the given tx should be sponsored, false otherwise
+// NOTE: The following requirements must be satisfied for fee sponsorship
+// 1. Fee sponsorship is enabled
+// 2. The tx is a BTCT transfer tx
+// 3. The gas fee does not exceed the maximum sponsorship fee
+// 4. The fee sponsor account has sufficient balances
+func (fsd FeeSponsorshipDecorator) checkSponsorship(ctx sdk.Context, tx sdk.Tx, fee sdk.Coins) bool {
+	if !fsd.btcbridgeKeeper.FeeSponsorshipEnabled(ctx) {
+		return false
+	}
+
+	if !fsd.isSponsorableTx(ctx, tx) {
+		return false
+	}
+
+	if !fee.IsAllLTE(fsd.btcbridgeKeeper.MaxSponsorFee(ctx)) {
+		return false
+	}
+
+	if !fsd.bankKeeper.SpendableCoins(ctx, fsd.getFeeSponsorAddress()).IsAllGTE(fee) {
+		return false
+	}
+
+	return true
+}
+
+// isSponsorableTx returns true if the given tx is sponsorable, false otherwise
+func (fsd FeeSponsorshipDecorator) isSponsorableTx(ctx sdk.Context, tx sdk.Tx) bool {
+	for _, m := range tx.GetMsgs() {
+		switch msg := m.(type) {
+		case *banktypes.MsgSend:
+			denoms := msg.Amount.Denoms()
+			if len(denoms) != 1 || denoms[0] != fsd.btcbridgeKeeper.BtcDenom(ctx) {
+				return false
+			}
+
+		case *banktypes.MsgMultiSend:
+			denoms := msg.Inputs[0].Coins.Denoms()
+			if len(denoms) != 1 || denoms[0] != fsd.btcbridgeKeeper.BtcDenom(ctx) {
+				return false
+			}
+
+		default:
+			return false
+		}
+	}
+
+	return true
+}
+
+// getFeeSponsorAddress gets the fee sponsor address
+func (fsd FeeSponsorshipDecorator) getFeeSponsorAddress() sdk.AccAddress {
+	return fsd.accountKeeper.GetModuleAddress(types.FeeSponsorName)
+}